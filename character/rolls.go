acef16ccf5c81f57d955158034c0bdd49c6a9c6f
the xhr <xhr@giessen.ccc.de>
2025-01-06 10:30:12 +0000

    Now for real

--- character/rolls.go
+++ character/rolls.go
@@ -4,7 +4,13 @@ import (
 	"math/rand"
 )
 
-func dice(side int) int {
-	r := rand.New(time.Now())
-	return r.Intn(side)
+func RollDice(number int, side int) []int {
+	result := make([]int, number)
+	for i := range number {
+		result[i] = rand.Intn(side) + 1
+	}
+
+	return result
 }
+
+
