d6c65243963417048097b478f8a57f78f3dbd1fd
the xhr <xhr@giessen.ccc.de>
2025-01-07 21:15:40 +0000

    Create new dir upon startup
    To save files later on

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"xosc.org/btwscrolls/cli"
@@ -19,6 +20,8 @@ func main() {
 
 	clog.DebugLog = true
 
+	setupBaseDir()
+
 	for {
 		rl.SetPrompt("> ")
 		line, err := rl.Readline()
@@ -34,5 +37,24 @@ func main() {
 
 		// XXX add history
 		cli.ExecuteCommand(cmd)
+	}
+}
+
+func setupBaseDir() {
+	var btwscrollsHome string
+	if xdgh := os.Getenv("XDG_CONFIG_HOME"); len(xdgh) > 0 {
+		btwscrollsHome = xdgh + "/btwscrolls"
+	} else if home := os.Getenv("HOME"); len(home) > 0 {
+		btwscrollsHome = home + "/.config/btwscrolls"
+	} else {
+		log.Fatal("Neither $XDG_CONFIG_HOME nor $HOME set")
+	}
+
+	if _, err := os.Stat(btwscrollsHome); err != nil {
+		if os.IsNotExist(err) {
+			if err := os.Mkdir(btwscrollsHome, 0755); err != nil {
+				log.Fatalf("Cannot create home dir: %v", err.Error())
+			}
+		}
 	}
 }
